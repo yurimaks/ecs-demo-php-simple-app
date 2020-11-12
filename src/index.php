ef17d7677e783877fad5f40486003fae35ad45c4
EC2 Default User <ec2-user@ip-172-31-15-81.eu-west-1.compute.internal>
2020-11-12 11:43:22 +0000

    new comit 3
    Signed-off-by: EC2 Default User <ec2-user@ip-172-31-15-81.eu-west-1.compute.internal>

--- src/index.php
+++ src/index.php
@@ -19,6 +19,7 @@
                 <h2>Congratulations</h2>
 <h2>AAAAA</h2>
 <h2>BBBBB</h2>
+<h2>CCCCC</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
