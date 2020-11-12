a8db2b4c7cef1d9db05d2d883cde744516bc57c3
EC2 Default User <ec2-user@ip-172-31-15-81.eu-west-1.compute.internal>
2020-11-12 09:58:20 +0000

    Change the background color to Red
    Signed-off-by: EC2 Default User <ec2-user@ip-172-31-15-81.eu-west-1.compute.internal>

--- src/index.php
+++ src/index.php
@@ -27,7 +27,7 @@
 
             </div>
         </div>
-
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
     </body>
